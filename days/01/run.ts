6ab66cd7d85a2402fad24752fd6cf74623f58e30
Anthony Murphy <anthonymurphy252@gmail.com>
2025-12-01 19:18:36 +0000

    Day 1

--- days/01/run.ts
+++ days/01/run.ts
@@ -1,3 +1,55 @@
-const lines = Deno.readTextFileSync("days/01/input.test.txt").split("\n");
+const lines = Deno.readTextFileSync("days/01/input.txt").split("\n");
 
-console.log(lines);
+type Direction = "L" | "R";
+
+const instructions = lines.map((line) => {
+  const direction = line[0] as Direction;
+  const magnitude = parseInt(line.slice(1));
+  return { direction, magnitude };
+});
+
+const modulo = (a: number, b: number) => {
+  const answer = a % b;
+  return answer >= 0 ? answer : answer + b;
+};
+
+const part1 = (() => {
+  let position = 50;
+  let zeroCount = 0;
+  instructions.forEach(({ direction, magnitude }) => {
+    switch (direction) {
+      case "L": {
+        position = modulo(position - magnitude, 100);
+        break;
+      }
+      case "R": {
+        position = modulo(position + magnitude, 100);
+      }
+    }
+    if (position === 0) zeroCount++;
+  });
+  return zeroCount;
+})();
+
+const part2 = (() => {
+  let position = 50;
+  let zeroCount = 0;
+  instructions.forEach(({ direction, magnitude }) => {
+    for (let i = 0; i < magnitude; i++) {
+      switch (direction) {
+        case "L": {
+          position = modulo(position - 1, 100);
+          break;
+        }
+        case "R": {
+          position = modulo(position + 1, 100);
+        }
+      }
+      if (position === 0) zeroCount++;
+    }
+  });
+  return zeroCount;
+})();
+
+console.log(part1);
+console.log(part2);
---
+++ utils/absurd.ts
@@ -0,0 +1,3 @@
+export const absurd = (_: never) => {
+  throw new Error("The absurd function was called");
+};
